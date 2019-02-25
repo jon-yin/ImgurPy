e41cf764c91dcb2a9c039bbcacfdd4f044d77ac9
Jonathan Yin <jonathan.yin.1@stonybrook.edu>
2019-02-25 03:13:45 -0500

    Progress on adding search result scraper

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import utils
 import requests
 
 test_file = "test.txt"
+TEST_URL = "https://imgur.com/search/time?q=spongebob&qs=thumbs"
+SCROLL = "https://imgur.com/search/time/all/page/1?scrolled&q=spongebob&q_size_is_mpx=off&qs=thumbs"
 
 def driver(source):
     ids = utils.fetch_image_ids(source)
@@ -10,10 +12,12 @@ def driver(source):
     posts = [Post(metadata) for metadata in metadatas]
     utils.download("./test_files/album1", *posts)
 
-with open(test_file) as fh:
-    body = fh.read()
+# with open(test_file) as fh:
+#     body = fh.read()
 
-body = str(requests.get(TEST_URL).content, encoding="utf-8")
-#with open("test_file_2.txt", "w")as fh:
-#    fh.write(body)
-driver(body)
+#body = str(requests.get(TEST_URL).content, encoding="utf-8")
+with open("test_file_search.txt", "r")as fh:
+    body = fh.read()
+handler = utils.SearchHandler()
+print(len(handler.fetch_image_ids(body)))
+#  driver(body)
--- utils.py
+++ utils.py
@@ -2,20 +2,49 @@ import re
 import json
 import requests
 import os.path, os
+from enum import Enum
 
-IMG_IDS = re.compile('''\{"hash":(.*?),''')
 ext_str = "(\{\"hash\":%s.*?\})"
 
+class AlbumHandler():
+    IMG_IDS = re.compile('''\{"hash":(.*?),''')
+    def fetch_image_ids(self, data):
+        return AlbumHandler.IMG_IDS.findall(data)
+    def fetch_metadata(self, data,id):
+        res = re.search(ext_str % id,data)
+        if res is None:
+            return None
+        else:
+            return json.loads(res.group(1))
 
-def fetch_image_ids(data):
-    return IMG_IDS.findall(data)
+class SearchHandler():
+    IMG_IDS = re.compile('''"/gallery/(.*?)"''')
+    PAGED_SEARCH_URL = "https://imgur.com/search/time/all/page/%s?q=spongebob&q_size_is_mpx=off&qs=thumbs"
 
-def fetch_metadata(data,id):
-    res = re.search(ext_str % id,data)
-    if res is None:
-        return None
-    else:
-        return json.loads(res.group(1))
+    def fetch_image_ids(self, data):
+        return SearchHandler().IMG_IDS.findall(data)
+
+    def fetch_metadata(self, ids):
+        pass
+
+    def fetch_additional_results(self):
+        pass
+
+class SortType(Enum):
+    TIME = "time"
+    SCORE = "score"
+    RELEVANCE = "relevance"
+
+class SearchQuery():
+    def __init__(self, type, terms):
+        self.type = type
+        self.terms = terms
+        self.page = 1
+
+
+
+def build_search_query(query : str):
+    return query.replace(" ", "_")
 
 def download(filepath, *args, multithread=False):
     if not os.path.exists(filepath):
